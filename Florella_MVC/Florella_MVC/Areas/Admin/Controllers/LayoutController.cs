07aa61922de9876a7286e22f5782758c1f48ffe6
Musa Musazada <musaam@code.edu.az>
2021-12-22 15:23:01 +0400

    Layout Delete and FileUpload Added

--- Florella_MVC/Florella_MVC/Areas/Admin/Controllers/LayoutController.cs
+++ Florella_MVC/Florella_MVC/Areas/Admin/Controllers/LayoutController.cs
@@ -1,7 +1,10 @@
 ﻿using Florella_MVC.DAL;
 using Florella_MVC.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Florella_MVC.Areas.Admin.Controllers
@@ -11,10 +14,11 @@ namespace Florella_MVC.Areas.Admin.Controllers
     {
 
         private readonly AppDbContext _context;
-
-        public LayoutController(AppDbContext context)
+        private readonly IWebHostEnvironment _env;
+        public LayoutController(AppDbContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
         public async Task<IActionResult> Index()
         {
@@ -35,6 +39,8 @@ namespace Florella_MVC.Areas.Admin.Controllers
 
             return View(layoutItem);
         }
+
+
         public async Task<IActionResult> Create()
         {
             return View();
@@ -44,14 +50,71 @@ namespace Florella_MVC.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Layout layout)
         {
+
+           
+
             if (!ModelState.IsValid)
             {
-                return View();
+                return View(layout);
             }
+
+            if (!layout.File.ContentType.Contains("image")) {
+                ModelState.AddModelError(nameof(layout.File), "File type is not supported");
+                return View(layout);
+            }
+
+            if(layout.File.Length > 1024 * 1000)
+            {
+                ModelState.AddModelError(nameof(layout.File), "Your file size is: " + layout.File.Length + " /n Please compress it!");
+                return View(layout);
+            }
+
+
+            string fileName = Guid.NewGuid() + layout.File.FileName;
+            string wwwRoot = _env.WebRootPath;
+
+            var path = Path.Combine(wwwRoot, "img", fileName);
+            FileStream fileStream = new FileStream(path, FileMode.Create);
+            await layout.File.CopyToAsync(fileStream);
+            fileStream.Close();
+
             await _context.Layout.AddAsync(layout);
             await _context.SaveChangesAsync();
 
             return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            Layout layout = await _context.Layout.FindAsync(id);
+
+            if (layout == null)
+            {
+                return NotFound();
+            }
+
+            return View(layout);
+
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+
+            Layout layout = await _context.Layout.FindAsync(id);
+
+            if (layout == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(layout);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
--- Florella_MVC/Florella_MVC/Models/Layout.cs
+++ Florella_MVC/Florella_MVC/Models/Layout.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Florella_MVC.Models
 {
@@ -13,5 +15,9 @@ namespace Florella_MVC.Models
         [MaxLength(60)]
         public string twitter_url { get; set; }
 
+        public string image { get; set; }
+        
+        [NotMapped]
+        public IFormFile File { get; set; }
     }
 }
